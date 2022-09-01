d02c10ddd0287b7e8fcc4b73bf8b5982b6ebd79c
coverdriver27 <72575840+coverdriver27@users.noreply.github.com>
2022-09-01 00:38:29 -0700

    Add files via upload

---
+++ Ex02/ex2.py
@@ -0,0 +1,10 @@
+# A comment, this is so you can read this line of the code later
+# Anything after the $ is ignored by python.
+
+print ("Bet you did not see the comments.")
+print ("I Could have code like this.") # and this comment after is ignored
+
+# You can also use a comment to "disable" or comment out a code :
+# print ("This will not run.")
+
+print ("This will run")

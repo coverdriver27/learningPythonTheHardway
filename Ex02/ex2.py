836b3accac3f97d6d94e25657c0008432b68c9c8
Dhulian Pereira <dhulian_sjc@hotmail.com>
2022-09-22 10:23:36 -0700

    Exercise 2 - Fixed code format

--- Ex02/ex2.py
+++ Ex02/ex2.py
@@ -1,13 +1,14 @@
 # A comment, this is so you can read this line of the code later
 # Anything after the $ is ignored by python.
 
-print ("Bet you did not see the comments.")
-print ("I Could have code like this.") # and this comment after is ignored
+print("Bet you did not see the comments.")
+print("I Could have code like this.")  # and this comment after is ignored
 
 # You can also use a comment to "disable" or comment out a code :
 # print ("This will not run.")
 
-print ("This will run")
-print ("Hi #there")
-print ("That prints ^")
-#The # in the code is inside a string ""
+print("This will run")
+print("Hi #there")
+print("That prints ^")
+
+# The # in the code is inside a string ""

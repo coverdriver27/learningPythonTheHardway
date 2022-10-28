c4630dd7d0bb443255310173e20487d3f12f6b8e
Dhulian Pereira <dhulian_sjc@hotmail.com>
2022-10-27 23:50:26 -0700

    Exercise 28 : Boolean Practise

--- Ex28/ex28.py
+++ Ex28/ex28.py
@@ -1,1 +1,12 @@
 # Exercise 28 : Boolean Practise
+
+"""
+Operator	Name	                Example	Try it
+==	        Equal	                    x == y
+!=	        Not equal	                x != y
+>	        Greater than	            x > y
+<	        Less than	                x < y
+>=	        Greater than or equal to	x >= y
+<=	        Less than or equal to	    x <= y
+"""
+

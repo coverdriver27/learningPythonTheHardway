1d7cc2f8e018679308abd756c8a7f8a353c6c220
Dhulian Pereira <dhulian_sjc@hotmail.com>
2022-10-28 00:03:06 -0700

    Exercise 28 : Boolean Practise

--- Ex28/ex28.py
+++ Ex28/ex28.py
@@ -10,3 +10,29 @@ Operator	Name	                Example	Try it
 <=	        Less than or equal to	    x <= y
 """
 
+print("is 10 equal to 100: ", 10 == 100)
+print("is 10 equal to 10: ", 10 == 10)
+print("is 10 not equal to 100: ", 10 != 100)
+print("is 10 not equal to 10: ", 10 != 10)
+print("is 10 greater to 100: ", 10 > 100)
+print("is 100 greater to 10: ", 100 > 10)
+print("is 10 less than 100: ", 10 < 100)
+print("is 100 less than 10: ", 100 < 10)
+print("is 10 greater than or equal to 100: ", 10 >= 100)
+print("is 10 greater than or equal to 10: ", 10 >= 10)
+print("is 10 less than or equal to 100: ", 10 <= 100)
+print("is 10 less than or equal to 10: ", 10 <= 10)
+
+# Output
+# is 10 equal to 100:  False
+# is 10 equal to 10:  True
+# is 10 not equal to 100:  True
+# is 10 not equal to 10:  False
+# is 10 greater to 100:  False
+# is 100 greater to 10:  True
+# is 10 less than 100:  True
+# is 100 less than 10:  False
+# is 10 greater than or equal to 100:  False
+# is 10 greater than or equal to 10:  True
+# is 10 less than or equal to 100:  True
+# is 10 less than or equal to 10:  True

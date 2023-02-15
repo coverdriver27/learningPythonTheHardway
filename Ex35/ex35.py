27c47d6019cbc3689f2d637456da26d24401736b
Dhulian Pereira <dhulian_sjc@hotmail.com>
2023-02-15 01:46:49 -0800

    Exercise 35 : Branches and Functions

--- Ex35/ex35.py
+++ Ex35/ex35.py
@@ -8,6 +8,7 @@ figure out what it's doing.
 
 from sys import exit
 
+# create a bunch of functions and define what they do
 def gold_room():
     print("This room is full of gold. How much do you want to take?")
 
@@ -30,6 +31,7 @@ def bear_room():
     print("How are you going to move the bear?")
     bear_moved = False
 
+    # While loop created for this function to do a certain thing, depending on the users input
     while True:
         choice = input("> ")
 
@@ -42,7 +44,7 @@ def bear_room():
         elif choice == "taunt bear" and bear_moved:
             dead("The bear gets pissed off and chews your leg off.")
         elif choice == "open door" and bear_moved:
-            gold_room()
+            gold_room()  # if the input matches "open door" this kicks off the gold_room function
         else:
             print("I got no idea what that means.")
 
@@ -60,25 +62,30 @@ def cthulhu_room():
     else:
         cthulhu_room()
 
+# this function replaces the given string from functions above and passes it to its parameter and prints out
+# also when the dead function is called the, the exit(0) terminates the game
 def dead(why):
     print(why, "Good job!")
     exit(0)
 
+# this function defines the start() of the game
 def start():
+    # Bunch of print statements after the game kicks off
     print("You are in a dark room.")
     print("There is a door to your right and left.")
     print("Which one do you take?")
 
-    choice = input("> ")
-
+    choice = input("> ")  # prompt the user to input a choice and save the value to the choice variable
+    # if-else statements on what to do depending on the user input
     if choice == "left":
-        bear_room()
+        bear_room()  # if user inputs left, that kicks off the bear_room function that was defined above
     elif choice == "right":
-        cthulhu_room()
+        cthulhu_room()  # if user inputs right, that kicks off the cthulhu_room function that was defined above
     else:
         dead("You stumble around the room until you starve.")
 
-start()
+
+start()  # this starts up the game, starting from the start() function defined on top
 
 # Output
 """
